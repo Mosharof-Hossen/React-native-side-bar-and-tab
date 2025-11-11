e73a84b4b13b903fef63c997848c56dcbaec88b0
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2025-11-11 17:17:06 +0600

    bug fixed

--- app/drawer/(tabs)/home.tsx
+++ app/drawer/(tabs)/home.tsx
@@ -1,12 +1,16 @@
+  import { useCurrentUser } from "@/store/features/auth/authSlice";
+import { useAppSelector } from "@/store/hooks";
 import { Text, View } from "react-native";
 
 const Home = () => {
-  return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Home</Text>
-    </View>
-  );
+  const user = useAppSelector(useCurrentUser);
+  console.log({ user });
+  return <View className="flex-1 justify-center items-center">
+    <Text>Welcome {user?.full_name}</Text>
+    <Text>Email: {user?.email}</Text>
+      <Text>Role: {user?.role}</Text>
+    <Text>Is Active: {user?.is_active ? "Yes" : "No"}</Text>
+    <Text>Is Email Verified: {user?.is_email_verified ? "Yes" : "No"}</Text>
+  </View>;
 };
-
 export default Home;
-
